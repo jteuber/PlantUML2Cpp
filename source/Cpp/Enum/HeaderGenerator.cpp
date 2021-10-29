432757284f9c295069bb4f33a8387a738359dec5
Jörn Teuber <jteuber@jaytea.org>
2021-10-29 23:58:43 +0200

    Implement Enum HeaderGenerator.

--- source/Cpp/Enum/HeaderGenerator.cpp
+++ source/Cpp/Enum/HeaderGenerator.cpp
@@ -1,11 +1,41 @@
 #include "Cpp/Enum/HeaderGenerator.h"
 
+#include <algorithm>
+#include <numeric>
+
 namespace Cpp::Enum {
 HeaderGenerator::HeaderGenerator(std::shared_ptr<Config> config)
     : m_config(std::move(config))
     , m_genUtils(m_config)
 {}
 
-std::string HeaderGenerator::generate(const Enum& in) {}
+std::string HeaderGenerator::generate(const Enum& in)
+{
+    std::string ret;
+
+    // pragme once
+    ret += "#pragma once\n\n";
+
+    // open namespaces
+    ret += m_genUtils.openNamespaces(in.namespaces);
+
+    // variant definition
+    ret += "enum class " + in.name + "{\n";
+
+    if (!in.enumerators.empty()) {
+        ret += std::accumulate(
+            ++in.enumerators.begin(),
+            in.enumerators.end(),
+            m_config->indent + in.enumerators.front().name,
+            [this](const std::string& l, const Enumerator& r) { return l + ",/n" + m_config->indent + r.name; });
+    }
+
+    ret += "\n};";
+
+    // close namespaces
+    ret += m_genUtils.closeNamespaces(in.namespaces);
+
+    return ret;
+}
 
 } // namespace Cpp::Enum
