9184b6d8bffab7fddb7061e109be24b718807d90
Jörn Teuber <jteuber@jaytea.org>
2023-05-06 23:21:04 +0200

    Hook up CLI11 with just path for now.

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,22 @@
 #include <filesystem>
 
+#include "CLI/App.hpp"
+#include "CLI/Formatter.hpp"
+#include "CLI/Config.hpp"
+
 #include "PlantUML2Cpp.h"
 
 int main(int argc, char** argv)
 {
+    CLI::App app{"App description"};
+
     std::filesystem::path path = std::filesystem::current_path();
-    if (argc >= 2) {
-        path = std::filesystem::path(argv[1]);
-    }
+    std::string pathString = path.string();
+    app.add_option("path", pathString, "Path of project directory containing the models in a folder called models");
+
+    CLI11_PARSE(app, argc, argv);
+
+    path = pathString;
 
     PlantUML2Cpp p;
     return p.run(path) ? 0 : -1;
